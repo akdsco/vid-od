6c306844ed34e1e0c2de800e2d6c20a0e857890a
Arkadiusz <arkadiusz@ostrowski.co>
2020-03-25 16:38:54 +0000

    add create, delete and update genre - add helper function validateGenre

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 // application
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+
 const genres = [
   {id: 1, name: 'Action'},
   {id: 2, name: 'Animation'},
@@ -31,20 +35,47 @@ app.get('/api/genres/:id', (req, res) => {
 
 // Create new genre
 app.post('/api/genres', (req, res) => {
+  const {error} = validateGenre(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
 
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  };
+
+  genres.push(genre);
+  res.send(genre);
 });
 
 // Update genre
 app.put('/api/genres/:id', (req, res) =>{
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('Genre with the given ID was not found.');
 
+  const {error} = validateGenre(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
 });
 
 // Delete genre
 app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send('Genre with the given ID was not found.');
 
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`)
-});
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(genre, schema);
+}
